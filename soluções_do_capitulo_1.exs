2b3e3b16e184386e5f2d9cae633f9207333ee964
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-10-10 20:42:39 -0300

    Add files via upload

---
+++ soluções_do_capitulo_1.exs
@@ -0,0 +1,18 @@
+# Run as: iex --dot-iex path/to/notebook.exs
+
+# Title: Soluções do Capitulo 1
+
+# ── Section ──
+
+# Respostas Capitulo 1:
+# Exercicio 1
+is_number(0)
+is_integer(1)
+is_float(0.5)
+is_binary("Zap")
+is_atom(:idade)
+is_boolean(true)
+is_map(%{nome: "João", idade: 30})
+is_tuple("Silva", 010, "coringa")
+
+# Exercicio 2
