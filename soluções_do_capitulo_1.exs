732cc1969e76e8e05b96f0b86be2a570e822b023
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-10-23 16:49:14 -0300

    Resolução Exercíco 2 soluções_do_capitulo_1.exs

--- soluções_do_capitulo_1.exs
+++ soluções_do_capitulo_1.exs
@@ -1,18 +1,32 @@
 # Run as: iex --dot-iex path/to/notebook.exs
+#Respostas adapatadas para a IDE Online Replit
 
 # Title: Soluções do Capitulo 1
 
 # ── Section ──
 
 # Respostas Capitulo 1:
-# Exercicio 1
-is_number(0)
-is_integer(1)
-is_float(0.5)
-is_binary("Zap")
-is_atom(:idade)
-is_boolean(true)
-is_map(%{nome: "João", idade: 30})
-is_tuple({"Silva", 010, "coringa"})
+#Exercicio 1
+IO.inspect(is_number(0))
+IO.inspect(is_integer(1))
+IO.inspect(is_float(0.5))
+IO.inspect(is_binary("Zap"))
+IO.inspect(is_atom(:idade))
+IO.inspect(is_boolean(true))
+IO.inspect(is_map(%{nome: "João", idade: 30}))
+IO.inspect(is_tuple{"Silva", 010, "coringa"})
 
-# Exercicio 2
+#Exercicio 2
+ocamilodev = 71255.53
+ocam_l = 30236.63
+camilotk = 5256.12
+
+media =  (ocamilodev + ocam_l + camilotk)/3
+
+if(floor(media) == 35582) do 
+  IO.puts("Parabéns você acertou)
+else
+  IO.puts("Erroooooou!)
+end
+
+#Exercício 3
